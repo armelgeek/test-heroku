a00fe8b34e69d59ccfbcd81f74b39decf55ac4ec
Armel Geek <armelgeek5@gmail.com>
2021-04-25 16:55:13 +0300

    correction de l'erreur de port sur heroku

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const port = process.env.PORT || 3000
 const app = express()
 app.get('/', function (req, res) {
  res.send('Hello World!')
 })
-app.listen(3000, function () {
+app.listen(port, function () {
  console.log('Votre app est disponible sur localhost:3000 !')
 })
